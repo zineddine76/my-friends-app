3f953954a6e0bfd1908ab193a3c6e7f7a2dd7243
Zineddine Haroune <Zineddine.Haroune@brinks.com>
2020-04-28 11:47:19 -0400

    Updates

---
+++ precache-manifest.a7c5d48fa1fea9a4ac4a2d80f3a3c2d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453f53b431d3e541afc73501a95c1d62",
+    "url": "/my-friends-app/index.html"
+  },
+  {
+    "revision": "e3bc9b56bbcfa3e9dd32",
+    "url": "/my-friends-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "2f6ef015c159e549fd90",
+    "url": "/my-friends-app/static/css/main.4166965b.chunk.css"
+  },
+  {
+    "revision": "e3bc9b56bbcfa3e9dd32",
+    "url": "/my-friends-app/static/js/2.1c09d554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-friends-app/static/js/2.1c09d554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6ef015c159e549fd90",
+    "url": "/my-friends-app/static/js/main.566e9ce9.chunk.js"
+  },
+  {
+    "revision": "943e516a6525a808b330",
+    "url": "/my-friends-app/static/js/runtime-main.8ae39689.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-friends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
